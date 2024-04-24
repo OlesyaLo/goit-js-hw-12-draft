30819569d4a10e308ae0321ac3e80b48b261039e
Olesya Lobodko <olesya.lobodko@gmail.com>
2024-04-24 04:53:47 +0200

    hm 12 done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,18 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '43527026-7407a6885caf79748912d7a12';
-export function serviceRequest(request) {
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: request,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
+export async function serviceRequest(request, page) {
+  const serviceResponse = await axios.get(`${BASE_URL}`, {
+    params: {
+      key: API_KEY,
+      q: request,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page,
+      per_page: 15,
+    },
   });
-
-  return fetch(`${BASE_URL}?${params}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
+  return serviceResponse.data;
 }
--- src/main.js
+++ src/main.js
@@ -14,6 +14,11 @@ import { createMarkup } from './js/render-functions';
 const searchForm = document.querySelector('.search-form');
 const galleryList = document.querySelector('.gallery');
 const loader = document.querySelector('.gallery-loader');
+const btnLoadMore = document.querySelector('.gallery-load-btn');
+let page = 1;
+let request = null;
+let totalPages;
+let limit = 15;
 
 const simpleLightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
@@ -23,12 +28,16 @@ const simpleLightbox = new SimpleLightbox('.gallery a', {
 
 searchForm.addEventListener('submit', onSearch);
 
-function onSearch(event) {
+async function onSearch(event) {
   event.preventDefault();
+
   loader.classList.toggle('loader');
+  btnLoadMore.classList.add('hidden');
   galleryList.innerHTML = '';
 
-  const request = event.target.elements.text.value.trim();
+  page = 1;
+
+  request = event.target.elements.text.value.trim();
 
   if (!request) {
     loader.classList.remove('loader');
@@ -37,28 +46,70 @@ function onSearch(event) {
     });
     return;
   }
+  try {
+    const response = await serviceRequest(request);
 
-  serviceRequest(request)
-    .then(response => {
-      if (response.total === 0) {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-      } else {
-        galleryList.innerHTML = createMarkup(response.hits);
-        simpleLightbox.refresh();
-      }
-    })
-    .catch(error => {
+    if (response.hits.length === 0) {
       iziToast.error({
         message:
-          'Oops! There was a problem loading the images. Please try again later!',
+          'Sorry, there are no images matching your search query. Please try again!',
       });
-      alert(error.message);
-    })
-    .finally(() => {
-      searchForm.reset();
-      loader.classList.toggle('loader');
+    } else {
+      galleryList.innerHTML = createMarkup(response.hits);
+      simpleLightbox.refresh();
+
+      totalPages = Math.ceil(response.totalHits / limit);
+      if (totalPages > 1) {
+        btnLoadMore.classList.remove('hidden');
+      }
+    }
+  } catch (error) {
+    iziToast.error({
+      message:
+        'Oops! There was a problem loading the images. Please try again later!',
     });
+    alert(error.message);
+  } finally {
+    searchForm.reset();
+    loader.classList.toggle('loader');
+  }
+}
+
+btnLoadMore.addEventListener('click', loadMore);
+
+async function loadMore() {
+  page += 1;
+  loader.classList.toggle('loader');
+
+  try {
+    const response = await serviceRequest(request, page);
+
+    galleryList.insertAdjacentHTML('beforeend', createMarkup(response.hits));
+    simpleLightbox.refresh();
+
+    const galleryElement = document.querySelector('.gallery');
+    const cardHeight =
+      galleryElement.firstElementChild.getBoundingClientRect().height;
+
+    window.scrollBy({
+      left: 0,
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+
+    if (totalPages === page) {
+      btnLoadMore.classList.add('hidden');
+      iziToast.info({
+        message: 'We are sorry, but you have reached the end of search results',
+      });
+    }
+  } catch (error) {
+    iziToast.error({
+      message:
+        'Oops! There was a problem loading the images. Please try again later!',
+    });
+    alert(error.message);
+  } finally {
+    loader.classList.toggle('loader');
+  }
 }
